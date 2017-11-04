30ccaac57ddf252cd11ad612ff2555c5b135664c
Umberto Calzari <umbertocalzari@me.com>
2017-11-04 19:38:07 +0100

    Initial Commit

---
+++ ProvaGit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProvaGit
+//
+//  Created by Umberto Calzari on 04/11/17.
+//  Copyright © 2017 prova. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProvaGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProvaGit
+//
+//  Created by Umberto Calzari on 04/11/17.
+//  Copyright © 2017 prova. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
